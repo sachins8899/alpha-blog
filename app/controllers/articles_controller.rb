cc8b5ae65d21f75aef5b7a769c1a681d1434ff4a
alpha blog <sachin.saini8899@gmail.com>
2018-12-17 17:31:58 +0530

    Add CURD operation in application

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,8 @@
 class ArticlesController < ApplicationController
-  
-  def new 
+  def new
     @article = Article.new
   end
-  
+
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -11,15 +10,46 @@ class ArticlesController < ApplicationController
       flash[:notice] ="Article Was Successfully Added !"
       redirect_to article_path(@article)
     else
+      flash[:error] ="Somthing went wrong !! "
       render 'new'
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article Updation Done !"
+      redirect_to article_path(@article)
+    else
+      flash[:error] = "Somthing went Wrong !!"
+      render 'edit'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def show
     @article = Article.find(params[:id])
   end
-  
-  private
-    def article_params
-      params.require(:article).permit(:title, :description)
+
+  def index
+    @article = Article.all
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if(@article.destroy)
+      flash[:notice] = "Article Deleted Successfully !"
+    else
+      flash[:error] = "Something went wrong !!"      
     end
+    redirect_to articles_path
+  end
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
+
+  validates :title ,length: {minimum: 2,maximum: 20}
+  validates :description,length: {minimum:5,maximum: 300}
 end
