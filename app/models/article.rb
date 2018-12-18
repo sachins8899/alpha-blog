2b021fe3c2ec55855e631ba0802b0b1be94a9ce6
alpha blog <sachin.saini8899@gmail.com>
2018-12-18 15:34:29 +0530

    All Bootstrap topices session 5 complete

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
 
-  validates :title ,length: {minimum: 2,maximum: 20}
-  validates :description,length: {minimum:5,maximum: 300}
+  validates :title ,length: {minimum: 2,maximum: 100}
+  validates :description,length: {minimum:5,maximum: 1000}
 end
