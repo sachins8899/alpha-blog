aa8c50868ccb8c607bdfb3804e03ffbdd4f33879
alpha blog <sachin.saini8899@gmail.com>
2018-12-20 18:03:08 +0530

    Add edit ,update,show user with articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,33 @@ class UsersController < ApplicationController
       flash[:success]="Successfully Added. Login with the user Name : #{@user.username}"
       redirect_to root_path
     else
-      flash[:dengar]="Somthig wrong !! "
+      flash[:danger]="Somthig went wrong !! "
+      render 'new'
     end
   end
   
+  def edit
+     @user = User.find(params[:id])
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def index
+    @user = User.all
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success]="User was updated successfully !!"
+      redirect_to articles_path
+    else
+      flash[:danger]="Somthig went wrong !! "
+      render 'edit'
+    end
+  end
   
   private
   
